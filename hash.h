1b0abe9145b34f6271737d1f1de94991e392f718
Step1st <Step1st@users.noreply.github.com>
2021-10-06 19:03:17 +0300

    Start

---
+++ hash.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <bitset>
+#include <vector>
+#include <random>
+#include <unordered_map>
+
+
+std::string bin_to_hex(std::string binary);
+std::string hash(std::string input, bool bin = false);
